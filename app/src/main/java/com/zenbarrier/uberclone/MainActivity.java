d4855f36555d24e31bbf1d9eef64d32a9f3dd678
ZenBarrier <anthony.j.barrera@gmail.com>
2016-10-30 17:48:12 -0400

    Anonymous login with driver/rider status saved to server.

--- app/src/main/java/com/zenbarrier/uberclone/MainActivity.java
+++ app/src/main/java/com/zenbarrier/uberclone/MainActivity.java
@@ -5,42 +5,49 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Switch;
 
 import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch switchRole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        switchRole = (Switch) findViewById(R.id.switchRole);
+
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void logIn(View view){
+        boolean isDriver = switchRole.isChecked();
+        ParseUser user = ParseUser.getCurrentUser();
+        user.put("isDriver", isDriver);
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e==null){
+                    Log.i("Sign", "Successful");
+                }
+                else{
+                    e.printStackTrace();
+                    Log.i("Sign", "Failed");
+                }
+            }
+        });
     }
 }
