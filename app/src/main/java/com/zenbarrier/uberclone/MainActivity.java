a1ddc5ef1823c9e04868feaf7fefb9416e0c2be8
ZenBarrier <anthony.j.barrera@gmail.com>
2016-10-30 17:48:56 -0400

    Removed unused imports.

--- app/src/main/java/com/zenbarrier/uberclone/MainActivity.java
+++ app/src/main/java/com/zenbarrier/uberclone/MainActivity.java
@@ -1,23 +1,15 @@
 package com.zenbarrier.uberclone;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Switch;
 
 import com.parse.ParseAnalytics;
-import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
-import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
-import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
